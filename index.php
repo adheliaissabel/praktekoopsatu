c62acff77d739a0d21677f46e31c602ac692196d
adheliaissabel <h1101241001@student.untan.ac.id>
2025-08-25 22:29:07 +0700

    tugas1

--- index.php
+++ index.php
@@ -12,12 +12,12 @@ $pesan = "Silakan klik tautan di bawah ini untuk pergi ke halaman Home dan Berbe
     <style>
         body { font-family: Arial, sans-serif; text-align: center; margin-top: 50px; }
         .container { max-width: 600px; margin: 0 auto; }
-        .box { background: #f0f8ff; padding: 20px; border-radius: 10px; margin-top: 15px; }
+        .box { background: #f0f8ff; padding: 10px; border-radius: 10px; margin-top: 15px; width: 300px; }
     </style>
 </head>
 <body>
     <div class="container">
-        <h1>SELAMAT DATANG DI TOKO <?php echo htmlspecialchars($nama); ?></h1>
+        <h1>SELAMAT DATANG DI SHOPEE?></h1>
         <p>Halo <strong><?= htmlspecialchars($nama) ?></strong></p>
         <p><?php echo $pesan; ?></p>
         <div class="box">
