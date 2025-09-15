8a3301a51557a06e3835ebfac06c26251e8ac3bb
adheliaissabel <h1101241001@student.untan.ac.id>
2025-09-15 13:25:53 +0700

    update

--- praktikum5.2.php
+++ praktikum5.2.php
@@ -9,17 +9,19 @@ class Mahasiswa{
   private $password;
 
 
+  // Constructor
+  public function __construct($nama, $nim, $prodi, $ipk, $password) {
+    $this->nama = $nama;
+    $this->nim = $nim;
+    $this->prodi = $prodi;
+    $this->ipk = $ipk;
+    $this->password = $password;
+  }
   protected function getNilaiIPK(){
     return "Nilai IPK = $this->ipk";
   }
   private function getPassword(){
     return "Password akun = $this->password";
-  }
-  function setNilaiIPK($ipk){
-    $this->ipk = $ipk;
-  }
-  function setPassword($password){
-    $this->password = $password;
   }
   public function tampilkanNilaiIPK(){
     return $this->getNilaiIPK();
@@ -29,20 +31,9 @@ class Mahasiswa{
   }
 }
 
-//buat objek
-$mahasiswa1 = new Mahasiswa();
-$mahasiswa1->nama = "Aurel";
-$mahasiswa1->nim = "H11012410";
-$mahasiswa1->prodi = "Sistem Informasi";
-$mahasiswa1->setNilaiIPK(3.89);
-$mahasiswa1->setPassword("57483293");
-
-$mahasiswa2 = new Mahasiswa();
-$mahasiswa2->nama = "Adhelia";
-$mahasiswa2->nim = "H1101241001";
-$mahasiswa2->prodi = "Sistem Informasi";
-$mahasiswa2->setNilaiIPK(4.00);
-$mahasiswa2->setPassword("50644303");
+//buat objek langsung dengan constructor
+$mahasiswa1 = new Mahasiswa("Aurel", "H11012410", "Sistem Informasi", 3.89, "57483293");
+$mahasiswa2 = new Mahasiswa("Adhelia", "H1101241001", "Sistem Informasi", 4.00, "50644303");
 
 echo "Nama: " . $mahasiswa1->nama . "\n" .
      "NIM: " . $mahasiswa1->nim . "\n" .
