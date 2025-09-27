a790eb548bf343c349c5a5c944c2e9d63a9af41b
adheliaissabel <h1101241001@student.untan.ac.id>
2025-09-27 21:02:17 +0700

    update

--- sistempesanmakan/autoload.php
+++ sistempesanmakan/autoload.php
@@ -1,22 +1,15 @@
 <?php
-// autoload.php → PSR-4 sederhana
-
 spl_autoload_register(function ($class) {
-    // hanya handle namespace "App\..."
-    $prefix = "App\\";
-    $base_dir = __DIR__ . "/";
+    $prefix = 'App\\';
+    $base_dir = __DIR__ . '/sistempesanmakan/App/Controller/MenuController.php';
 
-    // cek apakah class menggunakan prefix App\
     $len = strlen($prefix);
     if (strncmp($prefix, $class, $len) !== 0) {
         return;
     }
 
-    // ambil sisa namespace setelah "App\"
     $relative_class = substr($class, $len);
-
-    // ganti "\" dengan "/" → mapping ke file system
-    $file = $base_dir . str_replace("\\", "/", $relative_class) . ".php";
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
 
     if (file_exists($file)) {
         require $file;
--- sistempesanmakan/contoller/MenuController.php
+++ sistempesanmakan/contoller/MenuController.php
@@ -1,23 +1,23 @@
 <?php
 namespace App\Controller;
 
-use App\Repository\menurepository;
-use App\View\menuview;
-use App\Model\makanan;
-use App\Model\minuman;
+use App\Repository\MenuRepository;
+use App\View\MenuView;
+use App\Model\Makanan;
+use App\Model\Minuman;
 
 class MenuController {
-    private menurepository $repo;
-    private menuview $view;
+    private MenuRepository $repo;
+    private MenuView $view;
 
-    public function __construct(menurepository $repo, menuview $view) {
+    public function __construct(MenuRepository $repo, MenuView $view) {
         $this->repo = $repo;
         $this->view = $view;
     }
 
     public function seedData(): void {
-        $this->repo->tambah(new makanan("Nasi Goreng", 20000));
-        $this->repo->tambah(new minuman("Es Teh", 5000));
+        $this->repo->tambah(new Makanan("Nasi Goreng", 20000));
+        $this->repo->tambah(new Minuman("Es Teh", 5000));
     }
 
     public function tampilkanMenu(): void {
