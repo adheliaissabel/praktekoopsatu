691cfe1e4fa7d6ad60fe299600232f8944941c8e
adheliaissabel <h1101241001@student.untan.ac.id>
2025-09-27 21:24:02 +0700

    update

--- sistempesanmakan/autoload.php
+++ sistempesanmakan/autoload.php
@@ -1,22 +1,19 @@
 <?php
 spl_autoload_register(function ($class) {
-    // ubah namespace jadi path
-    $prefix = 'sistempesanmakan/Controller/MenuController.php';
+    $prefix = 'App\\';
     $base_dir = __DIR__ . '/';
 
-    // cek kalau class pakai prefix App\
     $len = strlen($prefix);
     if (strncmp($prefix, $class, $len) !== 0) {
         return;
     }
 
-    // ambil sisa nama class
     $relative_class = substr($class, $len);
-
-    // ganti \ dengan / supaya cocok folder
     $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
 
     if (file_exists($file)) {
         require $file;
+    } else {
+        echo "Autoload gagal cari file: $file\n"; // debug
     }
 });
