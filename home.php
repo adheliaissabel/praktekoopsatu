b4bd4993e5b61e451374fba214c34255d359af8b
adheliaissabel <h1101241001@student.untan.ac.id>
2025-09-27 20:23:30 +0700

    update

--- home.php
+++ home.php
@@ -137,7 +137,7 @@ $judul = "Eksplorasi Kode Bersama ADHELIA ISSABEL";
                 <span>26 September 2025</span>
             </div>
             <div class="link-group">
-                <a href="index2.php">Praktikum 6</a>
+                <a href="sistempesanmakan/index2.php">Praktikum 7</a>
             </div>
         </div>
     </div>
