adfdaf4abe8c85d7166a03db5068f021b55ec366
adheliaissabel <h1101241001@student.untan.ac.id>
2025-10-12 19:43:49 +0700

    update

--- sistempesanmakanan/App/Controller/MenuController.php
+++ sistempesanmakanan/App/Controller/MenuController.php
@@ -3,8 +3,11 @@ namespace App\Controller;
 
 use App\Repository\MenuRepository;
 use App\View\MenuView;
+use App\Traits\Loggable;  // <-- tambahkan ini
 
 class MenuController {
+    use Loggable;   // <-- tambahkan ini
+
     private MenuRepository $repo;
     private MenuView $view;
 
@@ -14,10 +17,12 @@ class MenuController {
     }
 
     public function seedData() {
+        $this->log("Seed data jalan");  // contoh pakai log
         echo "Seed data jalan\n";
     }
 
     public function tampilkanMenu() {
+        $this->log("Tampilkan menu jalan");  // contoh pakai log
         echo "Tampilkan menu jalan\n";
     }
 }
